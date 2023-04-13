57331027d38c2c79de95bd142bc91a1199373ff2
BuildTools <damian@famaf.unc.edu.ar>
2023-04-13 11:37:03 -0300

    Add tag version

--- build.sh
+++ build.sh
@@ -9,17 +9,19 @@ cd "$DIRNAME"
 
 OWNER=damian-barsotti
 
+VER=":3.3.1"
+
 IMAGE=docker-stacks-foundation
-docker build --tag $OWNER/$IMAGE -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
+docker build --tag $OWNER/$IMAGE$VER -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
 
 IMAGE=base-notebook
-docker build --tag $OWNER/$IMAGE -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
+docker build --tag $OWNER/$IMAGE$VER -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
 
 IMAGE=minimal-notebook
-docker build --tag $OWNER/$IMAGE -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
+docker build --tag $OWNER/$IMAGE$VER -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
 
 IMAGE=scipy-notebook
-docker build --tag $OWNER/$IMAGE -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
+docker build --tag $OWNER/$IMAGE$VER -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
 
 IMAGE=pyspark-notebook
-docker build --tag $OWNER/$IMAGE -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
+docker build --tag $OWNER/$IMAGE$VER -f "$DIRNAME/$IMAGE"/Dockerfile $IMAGE
